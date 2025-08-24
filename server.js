18f23fd2500acd53fced15cf6015be74c413da83
Nawodya Wijesinghe <165813638+NawodyaWijesinghe@users.noreply.github.com>
2025-08-24 12:59:22 +0530

    Cart

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import addressRoutes from "./Routes/cartroutes.js";
+
+
+dotenv.config();
+const app = express();
+
+// Middleware
+
+app.use(express.json());
+
+app.use("/api/Cart", cartroutes);
+
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI) 
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+// Example route
+app.get("/", (req, res) => {
+  res.send("Backend is running & MongoDB connected!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
